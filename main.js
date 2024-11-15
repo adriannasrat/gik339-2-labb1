d3b44d8f3497086c35242c6e11a258517d9581d8
BackolarsAnna <annabackolars@gmail.com>
2024-11-15 16:49:26 +0100

    Update main.js

--- main.js
+++ main.js
@@ -2,3 +2,10 @@ let checkbox = document.querySelector("input[name=divStyle]");
 const textFields = document.querySelectorAll(".textfield");
 const divElement = document.getElementById("divElement");
 const button = document.getElementById("button");
+
+textFields.forEach(el => {
+    el.addEventListener('input', e => {
+      console.log(e.target);
+    })
+  })
+  
