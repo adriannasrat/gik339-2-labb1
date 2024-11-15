fc7990d18244d4092c4dd8819c44dcedd7c7b462
Adrian <adriannasrat@gmail.com>
2024-11-15 16:47:13 +0100

    Added script and variables

---
+++ main.js
@@ -0,0 +1,4 @@
+let checkbox = document.querySelector("input[name=divStyle]");
+const textFields = document.querySelectorAll(".textfield");
+const divElement = document.getElementById("divElement");
+const button = document.getElementById("button");
